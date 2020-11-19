64042c2e7f8b506ad09a6188d64332a4005d95f9
Simon Mellerin <simon.mellerin@makina-corpus.com>
2020-11-19 12:18:16 +0100

    front - add link to document if preview failed

--- js/onlyoffice-preview.js
+++ js/onlyoffice-preview.js
@@ -9,10 +9,15 @@
             // Onlyoffice API is missing, the module
             console.log("Onlyoffice API can't be found, OnlyOffice Preview Field Module is not configured properly or Onlyoffice API can't be reached.");
 
+            var documentLink = document.createElement("a");
+            documentLink.appendChild(document.createTextNode(documentConfig.config.document.title));
+            documentLink.href = documentConfig.config.document.url;
+            errorMessage.classList.add("onlyoffice-preview-link");
+            document.getElementById(documentConfig.placeholder).appendChild(documentLink);
+
             var errorMessage = document.createElement("p");
             errorMessage.innerHTML = drupalSettings.onlyofficePreview.error_message;
             errorMessage.classList.add("onlyoffice-preview-error");
-
             document.getElementById(documentConfig.placeholder).appendChild(errorMessage);
           }
         }
